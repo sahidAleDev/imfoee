4da27ec3f326adee41b7102bec6962285dbf7f97
sahidAle <sahidalejandroo@gmail.com>
2023-05-28 19:09:00 -0600

    Sin vercel y link en los botones de registrarse

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,1 @@
 import { defineConfig } from 'astro/config';
-
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: vercel()
-});
